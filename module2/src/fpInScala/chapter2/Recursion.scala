efe9e36171fa0605369476be6e82b7c039c3ab25
Mark Perry <maperry78@yahoo.com.au>
2013-07-19 00:55:20 +1000

    Doing chapter one  talk

---
+++ module2/src/fpInScala/chapter2/Recursion.scala
@@ -0,0 +1,34 @@
+package com.github.mperry.fpInScala.chapter2
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: MarkPerry
+ * Date: 26/05/13
+ * Time: 10:09 PM
+ * To change this template use File | Settings | File Templates.
+ */
+object Recursion {
+
+	def main(s: Array[String]): Unit = {
+		println("hi world")
+		println(1.to(10).map{x => fib(x)})
+	}
+
+	def fib_naive(i: Int): Int = {
+		i match {
+			case 1 => 0
+			case 2 => 1
+			case _ => fib(i - 1) + fib(i - 2)
+		}
+	}
+
+	def fib(n: Int) = fib_tr(n, 1, 1)
+
+	def fib_tr(n: Int, a: Int, b: Int): Int = {
+		n match {
+			case 1 => b
+			case _ => fib_tr(n - 1, a + b, a)
+		}
+	}
+
+}
---
+++ module2/src/fpInScala/chapter3/List.scala
@@ -0,0 +1,52 @@
+package com.github.mperry.fpInScala.chapter3
+
+sealed trait List[+A] // `List` data type
+case object Nil extends List[Nothing] // data constructor for `List`
+case class Cons[+A](head: A, tail: List[A]) extends List[A]
+
+object List { // `List` companion object
+  def sum(ints: List[Int]): Int = ints match { // Pattern matching example
+    case Nil => 0
+    case Cons(x,xs) => x + sum(xs)
+  } 
+  
+  def product(ds: List[Double]): Double = ds match {
+    case Nil => 1.0
+    case Cons(0.0, _) => 0.0
+    case Cons(x,xs) => x * product(xs)
+  }
+  
+  def apply[A](as: A*): List[A] = // Variadic function syntax
+    if (as.isEmpty) Nil
+    else Cons(as.head, apply(as.tail: _*))
+  
+  val example = Cons(1, Cons(2, Cons(3, Nil))) // Creating lists
+  val example2 = List(1,2,3)
+  val total = sum(example)
+
+  val x = List(1,2,3,4,5) match {
+    case Cons(x, Cons(2, Cons(4, _))) => x
+    case Nil => 42 
+    case Cons(x, Cons(y, Cons(3, Cons(4, _)))) => x + y
+    case Cons(h, t) => h + sum(t)
+    case _ => 101 
+  }
+
+  def append[A](a1: List[A], a2: List[A]): List[A] =
+    a1 match {
+      case Nil => a2
+      case Cons(h,t) => Cons(h, append(t, a2))
+    }
+
+  def foldRight[A,B](l: List[A], z: B)(f: (A, B) => B): B = // Utility functions
+    l match {
+      case Nil => z
+      case Cons(x, xs) => f(x, foldRight(xs, z)(f))
+    }
+  
+  def sum2(l: List[Int]) = 
+    foldRight(l, 0.0)(_ + _)
+  
+  def product2(l: List[Double]) = 
+    foldRight(l, 1.0)(_ * _)
+}

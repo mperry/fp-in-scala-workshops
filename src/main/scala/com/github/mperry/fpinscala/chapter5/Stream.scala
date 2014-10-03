46ef6a3fcb3e250182fa8231ac263c83385efb5e
Mark Perry <maperry78@yahoo.com.au>
2014-10-03 17:37:26 +1000

    Finished chapter 5, strictness and laziness

--- src/main/scala/com/github/mperry/fpinscala/chapter5/Stream.scala
+++ src/main/scala/com/github/mperry/fpinscala/chapter5/Stream.scala
@@ -1,40 +1,27 @@
 package com.github.mperry.fpinscala.chapter5
 
-/**
- * Created with IntelliJ IDEA.
- * User: MarkPerry
- * Date: 1/09/13
- * Time: 12:18 AM
- * To change this template use File | Settings | File Templates.
- */
-sealed abstract class Stream[+A] {
-	def uncons: Option[Cons[A]]
-	def isEmpty: Boolean = uncons.isEmpty
+//import Stream._
 
-	def toList: List[A] = ???
-	def take(n: Int): Stream[A] = ???
-	def takeWhile(p: A => Boolean): Stream[A] = ???
+trait Stream[+A] {
+
+	def take(n: Int): Stream[A] = {
+		???
+	}
+
+	def toList: List[A] = {
+		???
+	}
+
+	def takeWhile(p: A => Boolean): Stream[A] = {
+		???
+	}
+
 }
-object Empty extends Stream[Nothing] {
-	val uncons = None
-}
-sealed abstract class Cons[+A] extends Stream[A] {
-	def head: A
-	def tail: Stream[A]
-	val uncons = Some(this)
-}
+
+case object Empty extends Stream[Nothing]
+case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
 object Stream {
-	def empty[A]: Stream[A] = Empty
-	def cons[A](h: => A, t: => Stream[A]): Stream[A] = {
-		new Cons[A] {
-			lazy val head = h
-			lazy val tail = t
-		}
-	}
-	def apply[A](as: A*): Stream[A] =
-		if (as.isEmpty)
-			Empty
-		else
-			cons(as.head, apply(as.tail: _*))
+
+
 }
